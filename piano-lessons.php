40101cfa67a7421ab69429bcc641ffc62e5ab57d
Jake Frondorf <jfrondorf@gmail.com>
2019-04-24 03:14:48 -0400

    Created folder structure, and main plugin files
    * piano-lessons.php
    * uninstall.php

---
+++ piano-lessons.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Piano Lessons Plugin
+ * Plugin URI:  http://jakefrondorf.com/plugins/piano-lessons
+ * Description: Plugin to show add custom lesson post type and display them
+ * Version:     .01
+ * Author:      Jake Frondorf
+ * Author URI:  https://jakefrondorf.com/
+ */
+
+
+?>
