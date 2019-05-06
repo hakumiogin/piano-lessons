ca467f0af03a1af64f3558d692f44baecb059663
Jake Frondorf <jfrondorf@gmail.com>
2019-05-06 03:49:53 -0400

    Remove various hooks and functions that will not be used in this plugin.

--- piano-lessons.php
+++ piano-lessons.php
@@ -21,7 +21,6 @@ function pl_init(){
 	pl_load_dependencies();
 	pl_set_admin_hooks();
 	pl_set_public_hooks();
-	pl_enque_scripts();
 	pl_enque_styles();
 }
 
@@ -38,26 +37,18 @@ function pl_set_admin_hooks(){
 
 	add_action('add_meta_boxes', 'meta_box_handler'); //metabox.php
 	add_action('save_post', 'pl_save_post'); //metabox.php
-
-	add_shortcode('lessons', 'lessons_shortcode'); //shortcode.php
 }
 
 function pl_set_public_hooks(){
-
+	add_shortcode('lessons', 'lessons_shortcode'); //shortcode.php
 }
 
-function pl_enque_scripts(){
-
-}
 
 function pl_enque_styles(){
 
 }
 
 pl_init();
-
-register_activation_hook( __FILE__, 'activate_pl_plugin' );
-register_deactivation_hook( __FILE__, 'deactivate_pl_plugin' );
 
 
 ?>
