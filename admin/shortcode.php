2a3356af699f8f3928a045773550b0b9f03ae2b9
Jake Frondorf <jfrondorf@gmail.com>
2019-05-06 03:54:42 -0400

    added basic functionality to the shortcode [lesson].

--- admin/shortcode.php
+++ admin/shortcode.php
@@ -3,8 +3,52 @@ if (!defined('WPINC')) {
 	die;
 }
 
+
 function lessons_shortcode($atts){
-	return "<h1>yolo</h1>";
+	$number_of_posts = apply_filters('pl_number_of_lessons_to_display', 10);
+	//By using a filter, it's easily changebale by future devs. Probably belongs in settings, but you didn't ask for a settings page
+	$args = array(
+		'numberposts' => $number_of_posts,
+		'orderby' => 'post_date',
+		'order' => 'DESC',
+		'post_type' => 'pl_lessons',
+	);
+	$recent_lessons = wp_get_recent_posts($args);
+
+
+	echo '<div class="lesson_container">';
+	foreach( $recent_lessons as $recent ){
+		$post_teachers = get_the_terms($recent["ID"], "pl_teacher");
+		$teacher = $post_teachers[0]->name;
+
+		$difficulty = get_post_meta($recent["ID"], 'pl_difficulty', true);
+
+		$thumbnail = get_the_post_thumbnail($recent["ID"], array(9999, 200), array( 'class' => 'lesson_thumbnail' ));
+
+
+		$post_genres = get_the_terms($recent["ID"], "pl_genre");
+		$genre_keys = array_keys($post_genres);
+		$last_key = end($genre_keys);
+		$genres = '';
+		foreach ($post_genres as $key => $term){
+			$genres = $genres.'<span class="genre">'.$term ->name."</span>";
+			$genres = $key === $last_key ? $genres : $genres. ", ";
+		}
+
+?>
+		<div class="lesson_item">
+			<?php echo $thumbnail; ?>
+			<div class="lesson_teacher"><?php echo $teacher;?></div>
+			<div class="lesson_difficulty"><?php echo $difficulty;?></div>
+			<div class="lesson_genres"><?php echo $genres;?></div>
+		</div>
+
+
+<?php
+	}
+	echo '</div>';
+	wp_reset_query();
+
 }
 
 ?>
